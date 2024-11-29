b358fc1ec40e98fd949ae0bc4c9804f07c6a7e6a
Fran Aragón <franaragondeveloper@gmail.com>
2024-11-29 14:53:30 +0100

    Fix render meetup test

--- src/components/meetups/MeetupItem.test.js
+++ src/components/meetups/MeetupItem.test.js
@@ -2,7 +2,13 @@
 import { shallow } from "enzyme";
 import MeetupItem from "./MeetupItem";
 
-test("<MeetupItem/> renders without crashing", () => {
-  const wrapper = shallow(<MeetupItem />);
+test("renders MeetupItem without crashing", () => {
+  const mockItem = {
+    image: "test-image.jpg",
+    title: "Test Meetup",
+    description: "This is a test meetup",
+  };
+
+  const wrapper = shallow(<MeetupItem item={mockItem} />);
   expect(wrapper.exists()).toBe(true);
 });
